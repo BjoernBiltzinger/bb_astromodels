2bdb79e48ce648ee5a9d0f0cd343fe0069d6d8da
grburgess <jmichaelburgess@gmail.com>
2021-03-18 13:09:45 +0100

    import directly

--- bb_astromodels/__init__.py
+++ bb_astromodels/__init__.py
@@ -1,6 +1,5 @@
-from .xray.absorption import absori, integrate_absori
-
-
 from ._version import get_versions
+from .xray.absorption import Absori, Integrate_Absori
+
 __version__ = get_versions()['version']
 del get_versions
